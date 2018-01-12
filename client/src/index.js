758f86be73fa5e3e781aab5be7411365924d78e0
Kevin Lee <timpaniman@gmail.com>
2018-01-12 11:37:04 -0800

    second

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,6 @@ import './index.scss';
 import App from './Views/app';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render( < App / > , document.getElementById('root'));
+
+//registerServiceWorker();
