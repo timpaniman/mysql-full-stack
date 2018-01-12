268125901385e469a8f5a9a8579c0d3fe3e4f9c0
Kevin Lee <timpaniman@gmail.com>
2018-01-12 11:45:10 -0800

    third

--- client/src/Views/app.js
+++ client/src/Views/app.js
@@ -5,18 +5,34 @@ import Organization from './Organization';
 import Person from './Person';
 import './app.scss';
 export default props =>
-<Router>
-  <div className="app">
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/organization">Organizations</Link></li>
-      <li><Link to="/person">Person</Link></li>
-    </ul>
 
-    <hr/>
+    <
+    Router >
+    <
+    div className = "app" >
+    <
+    ul >
+    <
+    li > < Link to = "/" > Home < /Link></li >
+    <
+    li > < Link to = "/organization" > Organizations < /Link></li >
+    <
+    li > < Link to = "/person" > Person < /Link></li >
+    <
+    /ul>
 
-    <Route exact path="/" component={Home}/>
-    <Route path="/organization" component={Organization}/>
-    <Route path="/person" component={Person}/>
-  </div>
-</Router>
+<
+hr / >
+
+    <
+    Route exact path = "/"
+component = { Home }
+/> <
+Route path = "/organization"
+component = { Organization }
+/> <
+Route path = "/person"
+component = { Person }
+/> <
+/div> <
+/Router>
